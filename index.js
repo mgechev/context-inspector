1a6a0f79096c0b9c3285bdf9758b9319b74550bd
mgechev <mgechev@gmail.com>
2025-08-08 15:37:14 -0700

    Increase the limit to 25mb of context

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.static('public'));
 
 // Store contexts with timestamps
